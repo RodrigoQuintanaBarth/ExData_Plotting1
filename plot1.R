3646ed2e8dede76f5c951b05de94b85f671ad435
RodrigoQuintanaBarth <rodrigo.quintana@abstracta.com.uy>
2017-06-20 20:24:16 -0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -37,8 +37,6 @@ data$Global_active_power <- as.numeric(data$Global_active_power)
 
 # PLOTTING
 
-par(mfrow=c(1,1))
-
 png("plot1.png",width = 480, height = 480, units = "px")
 
 hist(data$Global_active_power, col="red",xlab = "Global Active Power (kilowatts)", ylab="Frequency", main="Global Active Power")
